788456cecb1423c36297ca7cba106a5d3ed204f5
Randy Ramli <ramli.r@northeastern.edu>
2022-11-11 15:08:46 -0800

    initial commit

---
+++ src/test/java/com/example/swipeapi/SwipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
